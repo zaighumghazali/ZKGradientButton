c3e75369e1e3a4bd17aa823561691c9519fd6e03
Zaighum Ghazali Khan <zaighum.khan@arbisoft.com>
2023-04-01 16:41:01 +0500

    Initial Commit

---
+++ ZKGradientButton/ZKGradientButton.h
@@ -0,0 +1,18 @@
+//
+//  ZKGradientButton.h
+//  ZKGradientButton
+//
+//  Created by Zaighum on 4/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZKGradientButton.
+FOUNDATION_EXPORT double ZKGradientButtonVersionNumber;
+
+//! Project version string for ZKGradientButton.
+FOUNDATION_EXPORT const unsigned char ZKGradientButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKGradientButton/PublicHeader.h>
+
+
